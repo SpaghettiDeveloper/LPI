c4ba19dba46b84cff35fe2bcc113f7b0187b4cc4
SpaghettiDeveloper <stefano.peris.dev@gmail.com>
2016-10-28 10:33:51 +0200

    add about

--- lpi.py
+++ lpi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+# Schermata delle informazioni.
 def About():
     print("""
           _  (`-')  _
@@ -45,77 +45,76 @@ opzione = 0
 
 # Pulisce lo schermo ad ogni iterazione
 def Clearscreen():
-      os.system('clear')
+    os.system('clear')
 
 #===============================================================================
 
 while opzione:
+    print("""
+    *---------------------------------*
+    * LINUX POST INSTALLER - Ver. 1.0 *
+    *---------------------------------*
 
-      print("""
-      *---------------------------------*
-      * LINUX POST INSTALLER - Ver. 1.0 *
-      *---------------------------------*
+    +------------------ ABOUT -------------------+
+    + Autore: Stefano Peris <SpaghettiDeveloper> +
+    + eMail: stefano.peris.dev@gmail.com         +
+    +--------------------------------------------+
 
-      +------------------ ABOUT -------------------+
-      + Autore: Stefano Peris <SpaghettiDeveloper> +
-      + eMail: stefano.peris.dev@gmail.com         +
-      +--------------------------------------------+
-
-      +------------------------+
-      +--------- MENU ---------+
-      +------------------------+
+    +------------------------+
+    +--------- MENU ---------+
+    +------------------------+
 
 
-      [1] Python 3.x - pip + Pygame
-      [2] Midnight Commander
-      [3] Vim
-      [4] Git
-      [5] Wget
-      [6] Htop
-      [7] Zim
-      [8] Build Essential
-      [9] Geany
-      [i] Info
-      [x] Esci
-      """)
+    [1] Python 3.x - pip + Pygame
+    [2] Midnight Commander
+    [3] Vim
+    [4] Git
+    [5] Wget
+    [6] Htop
+    [7] Zim
+    [8] Build Essential
+    [9] Geany
+    [i] Info
+    [x] Esci
+    """)
 
-      opzione = input("Digita l'opzione desiderata > ")
+    opzione = input("Digita l'opzione desiderata > ")
 
-      if opzione == "1":
-          os.system("sudo apt-get install python3-pip")
+    if opzione == "1":
+        os.system("sudo apt-get install python3-pip")
 
-      elif opzione == "2":
-          os.system("sudo apt-get install -y mc")
+        elif opzione == "2":
+            os.system("sudo apt-get install -y mc")
 
-      elif opzione == "3":
-          os.system("sudo apt-get install -y vim")
+        elif opzione == "3":
+            os.system("sudo apt-get install -y vim")
 
-      elif opzione == "4":
-          os.system("sudo apt-get install -y git")
+        elif opzione == "4":
+            os.system("sudo apt-get install -y git")
 
-      elif opzione == "5":
-          os.system("sudo apt-get install -y wget")
+        elif opzione == "5":
+            os.system("sudo apt-get install -y wget")
 
-      elif opzione == "6":
-          os.system("sudo apt-get install -y htop")
+        elif opzione == "6":
+            os.system("sudo apt-get install -y htop")
 
-      elif opzione == "7":
-          os.system("sudo apt-get install -y zim")
+        elif opzione == "7":
+            os.system("sudo apt-get install -y zim")
 
-      elif opzione == "8":
-          os.system("sudo apt-get install -y build-essential cmake libsdl1.2-dev libboost-dev")
+        elif opzione == "8":
+            os.system("sudo apt-get install -y build-essential cmake libsdl1.2-dev libboost-dev")
 
-      elif opzione == "9":
-          os.system("sudo apt-get install -y geany")
+        elif opzione == "9":
+            os.system("sudo apt-get install -y geany")
 
-      elif opzione == "10":
-          os.system("sudo apt-get install -y gparted")
+        elif opzione == "10":
+            os.system("sudo apt-get install -y gparted")
 
-      elif opzione == "x":
-          exit()
+        elif opzione == "i":
+            About()
 
-      elif opzione == "i":
-          About()
+        elif opzione == "x":
+            exit()
 
-      else:
-          null
+        else:
+            null
